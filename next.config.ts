3c6c0e62da137cc9be1aa4bfc2750f96a45e0c78
Andrian <mabinas@tip.edu.ph>
2025-03-03 02:06:41 +0800

    try 1 for db

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  env: {
+    MONGODB_URI: process.env.MONGODB_URI,
+  },
   output: "standalone", // Important for AWS Amplify
   images: {
     unoptimized: true, // Required since Amplify does not support Next.js Image Optimization
