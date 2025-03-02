f7dbc37f1eb5751c60c0fb21328da5209f4547f8
Andrian <mabinas@tip.edu.ph>
2025-03-03 00:05:56 +0800

    update nextfile

--- codewithdre/next.config.ts
+++ codewithdre/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  images: {
+    unoptimized: true, // Ensures images work correctly on AWS Amplify
+  },
 };
 
 export default nextConfig;
