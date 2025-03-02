eef20039942e7c2c8eefda6953f443c03643ee77
Andrian <mabinas@tip.edu.ph>
2025-03-03 02:15:42 +0800

    removed the test api

--- app/api/test/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
-    res.status(200).json({ MONGODB_URI: process.env.MONGODB_URI || "Not Found" });
-  }
-  
